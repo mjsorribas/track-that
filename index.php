db9e2349f7126cbb9c7b0c45338079a6c0923e0d
sa7mon <dan.sa7mon@gmail.com>
2015-02-16 16:42:31 -0600

    Create index page.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+        index.php - The landing page of the system
+        Created on: 2/16/15
+        Created by: Dan
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
