6b25164624875e8d310f81f4f331e6c886b16b99
sa7mon <dan.sa7mon@gmail.com>
2015-02-17 14:05:47 -0600

    Add example navbar to start from

--- index.php
+++ index.php
@@ -8,10 +8,36 @@
     <head>
         <meta charset="UTF-8">
         <title>Inventory System</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">Project name</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">Settings</a></li>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Help</a></li>
+                  </ul>
+                  <form class="navbar-form navbar-right">
+                    <input type="text" class="form-control" placeholder="Search...">
+                  </form>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Scripts -->
+        <script type="text/javascript" src="./js/bootstrap-3.3.2.min.js"></script>
+        <script type="text/javascript" src="./js/jquery-1.11.2.min.js"></script>
     </body>
 </html>
