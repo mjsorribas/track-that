6f5d39eed3c7ae4db41e146d1bd4c57ae2496f26
sa7mon <dan.sa7mon@gmail.com>
2015-02-17 15:20:45 -0600

    Create styling for sidebar

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <meta charset="UTF-8">
         <title>Inventory System</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/sidebar.css">
     </head>
     <body>
         <!-- Start top Navbar -->
@@ -54,18 +55,18 @@
                         <li class="active"><a href="#">Analytics <span class="sr-only">(current)</span></a></li>
                         <li><a href="#">Export</a></li>
                     </ul>
-                  <ul class="nav nav-sidebar">
-                    <li><a href="">Nav item</a></li>
-                    <li><a href="">Nav item again</a></li>
-                    <li><a href="">One more nav</a></li>
-                    <li><a href="">Another nav item</a></li>
-                    <li><a href="">More navigation</a></li>
-                  </ul>
-                  <ul class="nav nav-sidebar">
-                    <li><a href="">Nav item again</a></li>
-                    <li><a href="">One more nav</a></li>
-                    <li><a href="">Another nav item</a></li>
-                  </ul>
+                    <ul class="nav nav-sidebar">
+                        <li><a href="">Nav item</a></li>
+                        <li><a href="">Nav item again</a></li>
+                        <li><a href="">One more nav</a></li>
+                        <li><a href="">Another nav item</a></li>
+                        <li><a href="">More navigation</a></li>
+                    </ul>
+                    <ul class="nav nav-sidebar">
+                        <li><a href="">Nav item again</a></li>
+                        <li><a href="">One more nav</a></li>
+                        <li><a href="">Another nav item</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
