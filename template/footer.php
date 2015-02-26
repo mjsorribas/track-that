2aec611aa42eb459c03d1961ca9d9280f3ca184b
sa7mon <dan.sa7mon@gmail.com>
2015-02-26 16:24:53 -0600

    Start work on accordian menu in sidebar

--- template/footer.php
+++ template/footer.php
@@ -4,6 +4,17 @@
         <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script> <!-- jQuery must be loaded first for Bootstrap to not complain -->
         <script type="text/javascript" src="js/bootstrap-3.3.2.min.js"></script>
         <script type="text/javascript" src="js/bootstrap-select.min.js"></script>
+        <script type="text/javascript">
+
+			$(document).ready(function(){
+			    $('ul.submenu').hide();
+			});
+
+			$('li.accordian').click(function() {
+			    $(this).find('ul').slideToggle();
+			});
+
+        </script>
     </body>
 </html>
 <!-- STOP TEMPLATE FOOTER -->
