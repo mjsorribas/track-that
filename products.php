63cae4f648ef72d957364e38d76fe68e93f0b73f
sa7mon <dan.sa7mon@gmail.com>
2015-03-03 11:25:20 -0600

    Replace sidebar with call to file.

--- products.php
+++ products.php
@@ -19,22 +19,7 @@
 	<div class="container-fluid"> <!-- container-fluid div should wrap everything under the top navbar -->
 	    <!-- Start left sidebar -->
 	    <div class="row">
-
-	        <div class="sidebar">
-	            <ul class="nav nav-sidebar">
-	                <li><a href="index.php">Overview</a></li>
-	                <li class="active"><a href="products.php">Products <span class="sr-only">(current)</span></a></li>
-	                <li class="accordian">
-	                	<a href="#">Projects</a>
-	                	<ul class="submenu">
-	                		<li><a href="#">Projects>Overview</a></li>
-	                		<li><a href="#">Projects>Add/Remove</a></li>
-	                		<li><a href="#">Projects>Change Status</a></li>
-	                	</ul>
-	                </li>
-	            </ul>
-	        </div>
-
+	    	<?php require_once("template/sidebar.php"); ?>
 	        <!-- End left sidebar -->
 			<div class="col-md-offset-2 maincontent">
 				<!-- Page content goes here -->
