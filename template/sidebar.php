31cf3c00bc6f05da414d96d858bc338697ff09d5
sa7mon <dan.sa7mon@gmail.com>
2015-03-03 12:17:26 -0600

    Add link to newly created page

--- template/sidebar.php
+++ template/sidebar.php
@@ -3,6 +3,7 @@
     <ul class="nav nav-sidebar">
         <li><a href="index.php">Overview</a></li>
         <li><a href="products.php">Products</a></li>
+        <li><a href="exportproducts.php">Products>Export</a></li>
         <li><a href="projects.php?view=active">Projects>Overview</a></li>
 		<li><a href="addproject.php">Projects>Add</a></li>
 		<li><a href="editproject.php">Projects>Change Status</a></li>
