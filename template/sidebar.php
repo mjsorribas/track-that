8043acc9492ced9ccb09e80350ed61cda5c6638a
sa7mon <dan.sa7mon@gmail.com>
2015-03-03 11:22:37 -0600

    Create template sidebar file to unify and simplify sidebar.

---
+++ template/sidebar.php
@@ -0,0 +1,9 @@
+<div class="sidebar">
+    <ul class="nav nav-sidebar">
+        <li><a href="index.php">Overview</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="projects.php?view=active">Projects>Overview</a></li>
+		<li><a href="addproject.php">Projects>Add</a></li>
+		<li><a href="#">Projects>Change Status</a></li>
+    </ul>
+</div>
