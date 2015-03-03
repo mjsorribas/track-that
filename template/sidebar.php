7f5d0f2817fe7f6f9f5a4f29f907f56b1e7302aa
sa7mon <dan.sa7mon@gmail.com>
2015-03-03 11:32:23 -0600

    Add link to editproject.php

--- template/sidebar.php
+++ template/sidebar.php
@@ -1,9 +1,11 @@
+<!-- Start template/sidebar.php -->
 <div class="sidebar">
     <ul class="nav nav-sidebar">
         <li><a href="index.php">Overview</a></li>
         <li><a href="products.php">Products</a></li>
         <li><a href="projects.php?view=active">Projects>Overview</a></li>
 		<li><a href="addproject.php">Projects>Add</a></li>
-		<li><a href="#">Projects>Change Status</a></li>
+		<li><a href="editproject.php">Projects>Change Status</a></li>
     </ul>
 </div>
+<!-- End template/sidebar.php -->
