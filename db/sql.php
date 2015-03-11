a5e9e9eebe7b510eba7b21719d048cdd8e1fdcec
sa7mon <dan.sa7mon@gmail.com>
2015-03-11 13:36:40 -0500

    Add sql settings file

---
+++ db/sql.php
@@ -0,0 +1,21 @@
+<!--  
+	sql.php - Contains all the MySQL settings and some functions.
+	Created by: Dan Salmon
+	Created on: 3/11/15
+-->
+<?php
+	// MySQL Settings
+	$servername = "localhost";
+	$user		= "root"; // WAMP: root
+	$password	= ""; 	  // WAMP: (blank)
+	$dbname		= "remoteinventory";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $user, $password,$dbname);
+
+	// Check connection
+	if (!$conn) {
+	    die("Connection failed: " . mysqli_connect_error());
+	}
+	// We've connected successfully!
+?>
