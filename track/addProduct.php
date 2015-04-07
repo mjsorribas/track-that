4a5301373fae813493b179c211e228811938a543
Dan Salmon <dan.sa7mon@gmail.com>
2015-04-06 19:01:12 -0500

    Fix scripts links

--- track/addProduct.php
+++ track/addProduct.php
@@ -11,17 +11,16 @@
 <head>
 	<title>Add Product</title>
 	<meta charset="UTF-8">
-    <link rel="stylesheet" href="backend/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../backend/css/bootstrap.min.css" />
 </head>
 <body>
 
 	<form method="POST" action="addProduct.php" id="frmAddProduct" role="form"> 							
-		<input name='inpProjId' id='inpProjId' type='hidden' form='frmUpdateProject'/>
-		<input class="btn btn-primary" type="submit" form="frmUpdateProject" value="Save" />
+		<input class="btn btn-primary" type="submit" form="frmAddProduct" value="Save" />
 	</form>
 
-	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+	<script type="text/javascript" src="../backend/js/jquery-1.11.2.min.js"></script>
 	<!-- jQuery must be loaded first for Bootstrap to not complain -->
-    <script type="text/javascript" src="js/bootstrap-3.3.2.min.js"></script>
+    <script type="text/javascript" src="../backend/js/bootstrap-3.3.2.min.js"></script>
 
 </body>
