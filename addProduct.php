4e5be7de385dc5e8007d727bf985303ab25d3143
sa7mon <dan.sa7mon@gmail.com>
2015-04-15 17:33:41 -0500

    Add PHP processing of form submission and SQL query creation.

--- addProduct.php
+++ addProduct.php
@@ -1,15 +1,15 @@
 <?php
-	/*
-	*
-	*	addProduct.php - The page for a user to add products remotely. The 
-	*					 whole point of this system.
-	*	Created: 4/6/15
-	*	Created by: Dan Salmon
-	*
-	*/
+/*
+*
+*	addProduct.php - The page for a user to add products remotely. The 
+*					 whole point of this system.
+*	Created: 4/6/15
+*	Created by: Dan Salmon
+*
+*/
 
-	require_once("template/security.php");
-	require_once("db/sql.php");
+require_once("template/security.php");
+require_once("db/sql.php");
 ?>
 <head>
 	<title>Add Product</title>
@@ -25,7 +25,27 @@
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
 </head>
 <body>
-	<?php require_once("template/navbar.php"); ?>
+	<?php 
+	require_once("template/navbar.php"); 
+
+	// Check if the form has been submitted before. If so, process the submission. 
+	$success = null;
+	if (isset($_POST['inpProdNum'])) {
+		// Form has been submitted. Process the submission.
+		$qryAddProduct = "INSERT INTO tbl_products (part_num,
+													added_by,
+													qty,
+													project_id,
+													added_on)
+						VALUES ('".$_POST['inpProdNum']."',
+								'".$_SESSION['userID']."',
+								'1',
+								'".$_POST['inpProject']."',
+								NOW())";
+		echo $qryAddProduct; 
+		//echo $_POST['inpProject'];
+	}
+	?>
 	
 	<div class="container center_container">
 	<!--div class="container center_container col-md-8 col-md-offset-2"-->
@@ -43,7 +63,7 @@
 					<div class="form-group">
 						<label for="inpProject" class="control-label">Project</label>
 						<div class="">
-							<select id="inpProject" class='selectpicker'>
+							<select id="inpProject" name="inpProject" class='selectpicker'>
 							<?php
 								// Query the db for all active projects.
 								$qryGetActiveProejects = "SELECT * FROM tbl_projects WHERE proj_status = 1";
@@ -53,9 +73,8 @@
 								// Loop through the results
 								while($row = mysqli_fetch_array($rsltActiveProjects)) {
 								// For each project, do the following
-									echo "<option>".$row['proj_name']."</option>\n";
+									echo "<option value=".$row['proj_id'].">".$row['proj_name']."</option>\n";
 								}
-
 							?>
 							</select>
 						</div>
@@ -70,5 +89,4 @@
 	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
 	<!-- jQuery must be loaded first for Bootstrap to not complain -->
     <script type="text/javascript" src="js/bootstrap-3.3.2.min.js"></script>
-
 </body>
