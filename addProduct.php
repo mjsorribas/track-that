d26c24d21a148d64321dc668fabec2db954036ad
sa7mon <dan.sa7mon@gmail.com>
2015-04-16 17:01:01 -0500

    Simplify and fix form layout and styling. Need to center form

--- addProduct.php
+++ addProduct.php
@@ -60,66 +60,60 @@ require_once("db/sql.php");
 	<!--div class="container center_container col-md-8 col-md-offset-2"-->
 		<div id="mainrow" class="row">
 			<?php 
-				if (!$success == null) {
-					// The query was attempted.
-					if ($success = true) {
-						// Query succeeded.
-						?>
-						<div class="alert alert-success alert-dismissible" role="alert">
-							<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-							<strong>Success!</strong> Product added to project.
-						</div>
-						<?php
-					} else {
-						// Query failed.
-						?>
-						<div class="alert alert-danger alert-dismissible" role="alert">
-							<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						    <strong>Error</strong> There was a problem adding the product to the project.
-						</div>
-						<?php
-					}
+			if (!$success == null) {
+				// The query was attempted.
+				if ($success = true) {
+					// Query succeeded.
+					?>
+					<div class="alert alert-success alert-dismissible" role="alert">
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<strong>Success!</strong> Product added to project.
+					</div>
+					<?php
+				} else {
+					// Query failed.
+					?>
+					<div class="alert alert-danger alert-dismissible" role="alert">
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					    <strong>Error</strong> There was a problem adding the product to the project.
+					</div>
+					<?php
 				}
+			}
 			?>
 			<form method="POST" action="addProduct.php" id="frmAddProduct" role="form" class="form-horizontal"> 	
-				<div class="row col-xs-10 col-xs-offset-1 col-lg-5">
-					<div class="form-group">
-						<label class="control-label" for="inpProdNum">Product Number</label>
-						<div class="">
-							<input type="text" class="form-control" id="inpProdNum" name="inpProdNum" placeholder="1234ABCD">
-						</div>
+				<div class="control-group">
+					<label class="control-label" for="inpProdNum">Product Number</label>
+					<div class="controls">
+						<input type="text" class="form-control" id="inpProdNum" name="inpProdNum" placeholder="1234ABCD">
 					</div>
 				</div>
-				<div class="row col-xs-10 col-xs-offset-1 col-lg-5">
-					<div class="form-group">
-						<label for="inpProject" class="control-label">Project</label>
-						<div class="">
-							<select id="inpProject" name="inpProject" class='selectpicker'>
-							<?php
-								// Query the db for all active projects.
-								$qryGetActiveProejects = "SELECT * FROM tbl_projects WHERE proj_status = 1";
-								// Execute query
-								$rsltActiveProjects = mysqli_query($conn, $qryGetActiveProjects);
+				<div class="control-group">
+					<label for="inpProject" class="control-label">Project</label>
+					<div class="controls">
+						<select id="inpProject" name="inpProject" class='selectpicker'>
+						<?php
+							// Query the db for all active projects.
+							$qryGetActiveProejects = "SELECT * FROM tbl_projects WHERE proj_status = 1";
+							// Execute query
+							$rsltActiveProjects = mysqli_query($conn, $qryGetActiveProjects);
 
-								// Loop through the results
-								while($row = mysqli_fetch_array($rsltActiveProjects)) {
-								// For each project, do the following
-									echo "<option value=".$row['proj_id'].">".$row['proj_name']."</option>\n";
-								}
-							?>
-							</select>
-						</div>
+							// Loop through the results
+							while($row = mysqli_fetch_array($rsltActiveProjects)) {
+							// For each project, do the following
+								echo "<option value=".$row['proj_id'].">".$row['proj_name']."</option>\n";
+							}
+						?>
+						</select>
 					</div>
 				</div>
-				<div class="row col-xs-10 col-xs-offset-1 col-lg-5">
-					<div class="form-group">
+				<div class="control-group">
 						<label for="inpQuantity" class="control-label">Quantity</label>
-						<div class="">
+						<div class="controls">
 							<input type="number" step="1" id="inpQuantity" name="inpQuantity" />
 						</div>
-					</div>
 				</div>
-				<div class="row col-xs-offset-4">
+				<div class="form-actions">
 					<input class="btn btn-primary" type="submit" form="frmAddProduct" value="Save" />
 				</div>
 			</form>
