3f7831a1f99717c0d4300097170077c2112c502c
sa7mon <dan.sa7mon@gmail.com>
2015-04-16 17:48:53 -0500

    Add dynamic link generation to list item

--- template/navbar.php
+++ template/navbar.php
@@ -15,7 +15,18 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hi, <?php echo $_SESSION['userName']."!";?> <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#"><i class="fa fa-sign-in"></i> Go To (Other Area)</a></li>
+                            <li>
+                            <?php
+                            // Dynamically generate link
+                            if (basename($_SERVER['PHP_SELF']) == "addproduct.php") {
+                                // We're on the addProduct page.
+                                echo "<a href='index.php'><i class='fa fa-sign-in'></i> Go to Management Area</a>";
+                            } else {
+                                // We're in the backend.
+                                echo "<a href='addproduct.php'><i class='fa fa-sign-in'></i> Go to Add Product Area</a>";
+                            }
+                            ?>
+                            </li>
                             <li class="divider"></li>
                             <li><a href="logout.php"><i class="fa fa-power-off"></i> Log Out</a></li>
                         </ul>
