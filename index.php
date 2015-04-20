28273625d79a1e5b2e7a6c8ac0640d8e60e4c97e
sa7mon <dan.sa7mon@gmail.com>
2015-04-20 18:49:00 -0500

    Create complex query to get all necessary data.

--- index.php
+++ index.php
@@ -15,7 +15,12 @@
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
     </head>
     <body>
-        <?php require_once("template/navbar.php"); ?>
+        <?php 
+            // INCLUDES
+            require_once("template/navbar.php"); 
+            require_once("template/includes.php");
+            require_once("db/sql.php");
+        ?>
         
         <div class="container-fluid"> <!-- container-fluid div should wrap everything under the top navbar -->
             <div class="row">
@@ -23,6 +28,19 @@
                 <?php 
                     $sidebarActivePage = "overview";
                     require_once("template/sidebar.php"); 
+
+                    // Get DB Data
+                    $qryGetLatestActivity = "SELECT prod.added_on AS occured, 'product_add' AS ActivityType, users.user_name, prod.added_by AS user_id, proj.proj_name, prod.project_id FROM tbl_products as 
+                                            prod INNER JOIN tbl_users AS users ON prod.added_by=users.user_id INNER JOIN tbl_projects AS proj ON prod.project_id=proj.proj_id UNION SELECT proj.updated_on AS 
+                                            occured, 'project_update' AS ActivityType, users.user_name, proj.updated_by AS user_id, proj.proj_name, proj.proj_id FROM tbl_projects as proj INNER JOIN tbl_users 
+                                            AS users ON proj.updated_by=users.user_id ORDER BY occured DESC LIMIT 20;";
+                    // Execute query
+                    $rsltGetLatestActivity = mysqli_query($conn, $qryGetLatestActivity);
+
+                    if (!$rsltGetLatestActivity) {
+                        die("Couldn't execute query on MySQL server.");
+                    }
+
                 ?>
                 <div class="col-md-offset-2 maincontent">
                     <h1 class="page-header">Overview</h1>
@@ -38,6 +56,17 @@
                             <th class="left">Time</th>
                             <th>Event</th>
                         </tr>
+                        <?php
+                            // Loop through the sql results
+                            while($row = mysqli_fetch_array($rsltGetLatestActivity)) {
+                                // For every result row, do the following.
+                                echo "<tr>";
+
+
+                                echo "</tr>";
+                            }
+                        ?>
+
                         <tr>
                             <td>2/18/15 10:28am</td>
                             <td>Ethan added an item to <a href="#">Alliance Insurance - 2 new cable drops</a></td>
